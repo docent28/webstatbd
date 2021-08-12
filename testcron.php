db3de6b97d30d0fd5d196d1a804c058a05ca8642
docent28 <eaa67@mail.ru>
2021-08-12 21:03:13 +0300

    Create testcron.php
    test планировщик

---
+++ testcron.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$database = "webstat";
+$username = "root";
+$password = "";
+// Устанавливаем соединение
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Проверяем соединение
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+
+for ($i=0; $i < 10; $i++) { 
+	$name = "'user.$i'";
+	$datestart = "'2021-08-04'";
+	$dateend = "'2021-08-19'";
+	$iduser = 1000 + $i;
+	$sql = "INSERT INTO tstcrn (name, datestart, dateend, iduser) VALUES ($name, $datestart, $dateend, $iduser)";
+	if (!mysqli_query($conn, $sql)) {
+	      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+ } 
+
+mysqli_close($conn);
+
+
+?>
