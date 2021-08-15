450e49ef34843e157afb2dd9e62eaf88b1c7e6a4
docent28 <eaa67@mail.ru>
2021-08-15 21:52:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
     echo('index.php<br>');
+
+$servername = "localhost";
+// $database = "docent28_tstcrn";
+// $username = "docent28_tstcrn";
+// $password = "Ntyfc1967!";
+$database = "webstat";
+$username = "root";
+$password = "";
+
+$start = microtime(true);
+
+// Устанавливаем соединение
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Проверяем соединение
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM `establishment`";
+$arrKeyAPI = mysqli_query($conn, $sql);
+
+
+echo 'Добавили мероприятия за ' . (microtime(true) - $start) . ' секунд<br>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
