223af409d2b2c49085acec9bde16d4128f79688f
docent28 <eaa67@mail.ru>
2021-08-25 15:17:46 +0300

    Update all_statistic.php

--- all_statistic.php
+++ all_statistic.php
@@ -1,3 +1,19 @@
+<?php
+session_save_path("session");
+session_start();
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 120)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+if (($_SESSION['user'] ?? '') === '') {
+    header('Location: /');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
